c1f34457de0af1689b7441c6cbd4d5f534879424
1nis <anis@kherraf.fr>
2025-03-25 10:12:05 +0100

    init ex2

---
+++ glq_tp4_ex2/src/adresses/AfficheurDAdressesImpl.java
@@ -0,0 +1,25 @@
+package adresses;
+/**
+ * Une implementation de l'interface <tt>AfficheurDAdresses</tt>.
+ *
+ */
+public class AfficheurDAdressesImpl implements AfficheurDAdresses {
+	private ChercheurDAdresses chercheur;
+	
+	public AfficheurDAdressesImpl(ChercheurDAdresses c) { 
+		chercheur = c; 
+	}
+	
+	@Override
+	public String afficher(String nom) {
+		Adresse a = chercheur.chercher( nom);
+		if ( a == null) throw new IllegalArgumentException();
+		String resultat = a.nom() + "\n";
+		int num = a.numero();
+		if ( num != 0) resultat += num +  " "; // un seul appel à numero()
+		//if ( a.numero() != 0) resultat += a.numero() +  " "; // deux appels à numero()
+		resultat += a.rue() + "\n";
+		resultat += a.codePostal() +  " " + a.ville();
+		return resultat;
+	}
+}
