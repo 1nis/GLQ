2eb193dc97b26a88bb842fc3e301a52e2b63f8a3
1nis <anis@kherraf.fr>
2025-03-25 11:29:44 +0100

    Update AfficheurDAdressesTest.java

--- glq_tp4_ex2/src/adresses/AfficheurDAdressesTest.java
+++ glq_tp4_ex2/src/adresses/AfficheurDAdressesTest.java
@@ -1,19 +1,30 @@
 package adresses;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AfficheurDAdressesTest {
 
-    private Adresse uneadresse;
-    private AfficheurDAdressesImpl afficheadresse;
+    private AfficheurDAdresses uneadresse;
+    private ChercheurDAdresses chercheAdresse;
 
     @BeforeEach
     void setup() throws Exception {
-        uneadresse = mock(Adresse.class);
-        afficheadresse = mock(AfficheurDAdressesImpl.class);
+        uneadresse = mock(AfficheurDAdresses.class);
+        chercheAdresse = mock(ChercheurDAdresses.class);
+        Adresse adresse1 = new Adresse("Anis", 92, "Boulevard Larrat", 13010, "Marseille");
+        when(chercheAdresse.chercher("Anis")).thenReturn(adresse1);
 
+        uneadresse = new AfficheurDAdressesImpl(chercheAdresse);
+    }
+
+    @Test
+    void testNom() {
+        System.out.println();
+        System.out.println(uneadresse.afficher("Anis"));
     }
 
 }
